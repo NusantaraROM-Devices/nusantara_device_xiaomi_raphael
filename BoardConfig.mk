07b29ea8ec631ea89a746a5ae671644bc5245ae9
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-01-15 10:07:14 +0000

    raphael: Use device sepolicy from device/xiaomi/raphael-sepolicy
    Change-Id: I6151bd94747bf8c0654f12e1b491350454f0c7dc
    
    raphael: Move sepolicy back to the device tree
    
    Imported from https://github.com/sashascurtu/android_device_xiaomi_raphael-sepolicy
    Also removed the use of deprecated flags (BOARD_SEPOLICY_DIRS,
    BOARD_PLAT_**) and used new ones instead (BOARD_VENDOR_SEPOLICY_DIRS,
    SYSTEM_EXT_**).
    
    Co-authored-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Change-Id: I0ceb82a62d6f50d5580b1761ff123438d5c24dc5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -230,7 +230,7 @@ SELINUX_IGNORE_NEVERALLOWS := true
 endif
 
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-include device/xiaomi/raphael-sepolicy/raphael-sepolicy.mk
+include device/xiaomi/raphael/sepolicy/raphael-sepolicy.mk
 
 # Vendor init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_raphael

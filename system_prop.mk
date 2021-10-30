9751d072a00d69e2f581438d0ee325ee5d0d00ee
Nirmal Abraham <x@x.x>
2021-10-31 00:13:02 +0200

    raphael: Disable debug.sf.recomputecrop Disable recompute crop change in SF

--- system_prop.mk
+++ system_prop.mk
@@ -62,7 +62,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
     debug.sf.enable_gl_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
-    debug.cpurend.vsync=false
+    debug.cpurend.vsync=false \
+    debug.sf.recomputecrop=0
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
